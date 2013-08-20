9fd03745effec7302adce63d22e830bfae0a4081
Christopher Su <christophersu9@gmail.com>
2013-08-20 20:51:30 +0800

    working!

---
+++ project-euler-offline.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+'''
+project-euler-offline.py
+Christopher Su
+Template for Python scripts loading JSON files.
+'''
+
+import os
+import json
+from pyDes import *
+
+def loadJSON(jsonStr):
+    try:
+        data = json.loads(jsonStr)
+    except ValueError:
+        logging.exception("Error parsing %s." % json_file)
+        sys.exit(1)
+    return data
+
+def main():
+    dir = os.path.dirname(__file__)
+    txtFile = open(os.path.join(dir, "solutions-encrypted"), "r")
+    txtStr = txtFile.read()
+    txtFile.close()
+    plain_text = triple_des('03b5660c7c16a07b').decrypt(txtStr, padmode=2)
+    solutions = loadJSON(plain_text)
+
+    current = raw_input("What problem are you currently working on? ")
+
+    while True:
+        proposed = raw_input("\nEnter solution: ")
+        if proposed == "exit":
+            break
+        elif proposed == solutions[current]:
+            print "Correct!"
+            current = raw_input("\nWhat problem are you working on? ")
+            if current == "exit":
+                break
+        else:
+            print "Sorry, that is incorrect."
+
+if __name__ == "__main__":
+    dir = os.path.dirname(__file__)
+    main()
